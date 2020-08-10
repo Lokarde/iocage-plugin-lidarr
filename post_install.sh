8d3bf96fb5b723453b047b9b3b8af31c522fc0e8
Hugo Lecourt <lecouhu1@Hugo-MacBookPro.lokarde.intranet>
2020-08-10 21:50:35 +0200

    Init files

---
+++ post_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sysrc -f /etc/rc.conf lidarr_enable="YES"
+
+#62991
+chown -R lidarr:lidarr /usr/local/share/lidarr
+
+#we need write permission to be able to write plugins update. #53127
+chmod 755 /usr/local/lidarr
+
+# Start the services
+service lidarr start
