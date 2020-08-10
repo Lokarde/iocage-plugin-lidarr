1cf1169069013a46f3d1d3ae9f3fe726463c8b46
Hugo Lecourt <lecouhu1@Hugo-MacBookPro.lokarde.intranet>
2020-08-10 22:54:41 +0200

    fix chmod lidarr folder

--- post_install.sh
+++ post_install.sh
@@ -5,7 +5,7 @@ sysrc -f /etc/rc.conf lidarr_enable="YES"
 chown -R lidarr:lidarr /usr/local/share/lidarr
 
 #we need write permission to be able to write plugins update. #53127
-chmod 755 /usr/local/lidarr
+chmod 755 /usr/local/share/lidarr
 
 # Start the services
 service lidarr start
